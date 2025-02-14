a8313874156dba47cb402bfed46bcfd2613c6547
ThiagoAndrea <a.paolini001@studenti.unibs.it>
2025-02-14 23:55:05 +0100

    Finito layout partita

---
+++ app/src/main/java/com/example/youcoach/Partita.kt
@@ -0,0 +1,21 @@
+package com.example.youcoach
+
+import java.io.Serializable
+
+data class Partita(
+    val id: String = "",
+    val orario: String = "",
+    val luogo: String = "",
+    val avversario: String = "",
+    val risultato: String = "",
+    val competizione: String = "",
+    val numero_tempi: Int = 0,
+    val minuti_per_tempo: Int = 0,
+    val numero_calciatori: Int = 0,
+    val casa: Boolean = false,
+    val modulo: String = "",
+    val convocati: Map<String, Int> = emptyMap(),
+    val titolari: Map<String, String> = emptyMap(),
+
+
+) : Serializable
